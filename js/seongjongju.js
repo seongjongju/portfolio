b7a7f4966334aa2dd3afea83bb8e4e97c8d26c63
seongjongju <sjj990480@gmail.com>
2024-10-01 17:34:23 +0900

    디자인 넣어야하고 컨택트 만들어야 함

---
+++ js/seongjongju.js
@@ -0,0 +1,12 @@
+const header = document.querySelector("header");
+console.log(header)
+
+window.addEventListener('scroll', function() {
+    let windowScrollY = window.scrollY;
+    console.log(windowScrollY)
+    if(windowScrollY > 1080) {
+        header.style.color = "#000"
+    } else {
+        header.style.color = "#fff"
+    }
+})
