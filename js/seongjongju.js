5adeeb4654a9943abe3e1700e27b5a92c6c48c47
seongjongju <sjj990480@gmail.com>
2024-10-13 20:01:02 +0900

    인덱스 페이지수정

--- js/seongjongju.js
+++ js/seongjongju.js
@@ -1,13 +1,1 @@
-const header = document.querySelector("header");
-console.log(header)
-
-window.addEventListener('scroll', function() {
-    let windowScrollY = window.scrollY;
-    console.log(windowScrollY)
-    if(windowScrollY > 1080) {
-        header.style.color = "#000"
-    } else {
-        header.style.color = "#fff"
-    }
-});
 var swiper = new Swiper(".design_Swiper", {});
