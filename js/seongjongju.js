a85412cdff36187ed1d3e73552e0fa821e1f1d9b
seongjongju <sjj990480@gmail.com>
2024-10-14 17:58:43 +0900

    반응형 430 중

--- js/seongjongju.js
+++ js/seongjongju.js
@@ -1,1 +1,37 @@
+$(document).ready(function() {
+    const header = $('header');
+    const closeBtn = $('.closeBtn');
+    const openBtn = $('.openBtn');
+
+    openBtn.on('click', function(e) {
+        e.preventDefault();
+        header.fadeIn();
+    })
+
+    closeBtn.on("click", function(e) {
+        e.preventDefault();
+        header.fadeOut();
+    });
+});
+
+const menus = document.querySelectorAll('nav > a');
+console.log()
+
+window.addEventListener('scroll', function(e) {
+    e.preventDefault();
+    const openBtn = document.querySelector('.openBtn');
+    let windowY = window.scrollY;
+
+    if(windowY > 832) {
+        openBtn.style.color = '#000'
+    } else {
+        openBtn.style.color = '#fff'
+    }
+})
+    
 var swiper = new Swiper(".design_Swiper", {});
+
+
+
+
+
